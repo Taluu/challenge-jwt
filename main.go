56da858ddcdc843f552ffdbd9bd59c650f583770
Baptiste Clavié <clavie.b@gmail.com>
2022-03-17 12:59:25 +0100

    Implement Create operation

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/Taluu/gabsee-test/generated/infrapb"
 	"google.golang.org/grpc"
@@ -30,6 +33,10 @@ func main() {
 
 	log.Fatalln(server.Serve(listener))
 }
+
+const (
+	defaultExpirationDuration = "86400s" // +1 day
+)
 
 // Service is the service that allow to interact with stored secrets through gRPC.
 type Service struct {
@@ -74,4 +81,44 @@ func (s *Service) Delete(ctx context.Context, in *infrapb.Secret) (*infrapb.Empt
 	}
 
 	return &infrapb.Empty{}, nil
+}
+
+func (s *Service) Create(ctx context.Context, in *infrapb.Secret) (*infrapb.Secret, error) {
+	if contains, _ := s.store.Contains(ctx, in.Name); contains {
+		return in, status.Errorf(codes.AlreadyExists, "secret name \"%s\" already exists", in.Name)
+	}
+
+	if in.Claims == nil {
+		in.Claims = make(map[string]string)
+	}
+
+	if _, ok := in.Claims["exp"]; !ok {
+		expirationDate, _ := time.ParseDuration(defaultExpirationDuration)
+		in.Claims["exp"] = fmt.Sprint(time.Now().Add(expirationDate).Unix())
+	}
+
+	unix, err := strconv.Atoi(in.Claims["exp"])
+
+	if err != nil {
+		return in, status.Errorf(codes.InvalidArgument, "error when parsing time for the expiration date : %s", err)
+	}
+
+	expirationDate := time.Unix(int64(unix), 0)
+
+	//TODO: generate jwt token
+
+	err = s.store.Save(
+		ctx,
+		Secret{
+			Name:      in.Name,
+			Claims:    in.Claims,
+			ExpiresAt: expirationDate,
+		},
+	)
+
+	if err != nil {
+		return in, status.Errorf(codes.Internal, "couldn't create secret : %s", err)
+	}
+
+	return in, nil
 }
--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 	"sync"
@@ -82,7 +83,7 @@ func TestList(t *testing.T) {
 	_, err := client.List(ctx, &infrapb.Empty{})
 
 	if err != nil {
-		t.Fatalf("Unexpected error : %s", err)
+		t.Fatalf("Unexpected error : %e", err)
 	}
 }
 
@@ -110,7 +111,7 @@ func TestDelete(t *testing.T) {
 	)
 
 	if err != nil {
-		t.Fatalf("Unexpected error : %s", err)
+		t.Fatalf("Unexpected error : %e", err)
 	}
 
 	if contains, _ := store.Contains(ctx, "foo"); contains {
@@ -137,6 +138,104 @@ func TestDeleteUnknownSecret(t *testing.T) {
 	)
 
 	if err != nil {
-		t.Fatalf("Unexpected error : %s", err)
+		t.Fatalf("Unexpected error : %e", err)
 	}
+}
+
+func TestCreate(t *testing.T) {
+	store := NewSecretStore()
+	conn := newTestConnection(t, store)
+
+	conn.Start()
+	defer conn.Stop()
+
+	ctx, cancel := newTestContext()
+	defer cancel()
+
+	// fixture
+	store.Save(ctx, Secret{
+		Name: "already existing",
+	})
+
+	t.Run("nominal", func(t *testing.T) {
+		client := infrapb.NewSecretsClient(conn.Dial(ctx))
+		_, err := client.Create(
+			ctx,
+			&infrapb.Secret{
+				Name: "nominal",
+			},
+		)
+
+		if err != nil {
+			t.Fatalf("Unexpected error : %s", err)
+		}
+
+		if contains, _ := store.Contains(ctx, "nominal"); !contains {
+			t.Fatalf("Secret not stored")
+		}
+	})
+
+	t.Run("with an already existing Secret", func(t *testing.T) {
+		client := infrapb.NewSecretsClient(conn.Dial(ctx))
+		_, err := client.Create(
+			ctx,
+			&infrapb.Secret{
+				Name: "already existing",
+			},
+		)
+
+		if err == nil {
+			t.Fatal("Should not be able to store an already stored secret")
+		}
+	})
+
+	t.Run("with expiration date", func(t *testing.T) {
+		timeIn10s := time.Now().Unix() + 10
+
+		client := infrapb.NewSecretsClient(conn.Dial(ctx))
+		_, err := client.Create(
+			ctx,
+			&infrapb.Secret{
+				Name: "valid expiration date",
+				Claims: map[string]string{
+					"exp": fmt.Sprint(timeIn10s),
+				},
+			},
+		)
+
+		if err != nil {
+			t.Fatalf("Unexpected error : %s", err)
+		}
+
+		secret, err := store.Fetch(ctx, "valid expiration date")
+
+		if err != nil {
+			t.Fatalf("could not fetch Secret")
+		}
+
+		if secret.ExpiresAt.Unix() != timeIn10s {
+			t.Fatalf("Secret expiration not properly overwritten")
+		}
+	})
+
+	t.Run("with invalid expiration date", func(t *testing.T) {
+		client := infrapb.NewSecretsClient(conn.Dial(ctx))
+		_, err := client.Create(
+			ctx,
+			&infrapb.Secret{
+				Name: "valid expiration date",
+				Claims: map[string]string{
+					"exp": "foo bar baz",
+				},
+			},
+		)
+
+		if err == nil {
+			t.Fatal("Expected a invalid argument error, got none")
+		}
+
+		if contains, _ := store.Contains(ctx, "foo"); contains {
+			t.Fatalf("Secret was stored anyway, shouldn't be the case")
+		}
+	})
 }
