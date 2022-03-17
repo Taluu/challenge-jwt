ce837d0abe7bc96f7b9ae303f65f3779fdb04daa
Baptiste Clavié <clavie.b@gmail.com>
2022-03-18 00:17:32 +0100

    Add possibility to setup through env vars

--- pkg/main.go
+++ pkg/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"net"
+	"os"
+	"time"
 
 	"github.com/Taluu/gabsee-test/generated/infrapb"
 	"github.com/Taluu/gabsee-test/pkg/secrets"
@@ -17,15 +19,40 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	server := grpc.NewServer()
+	config := secrets.Config{}
 
-	infrapb.RegisterSecretsServer(
-		server,
-		secrets.NewService(
-			secrets.NewSecretStore(),
-			secrets.Config{},
-		),
-	)
+	if TTL, ok := os.LookupEnv("SECRETS_TTL"); ok {
+		config.TTL, err = time.ParseDuration(TTL)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if NearTTL, ok := os.LookupEnv("SECRETS_NEAR_TTL"); ok {
+		config.NearTTL, err = time.ParseDuration(NearTTL)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if tickDuration, ok := os.LookupEnv("SECRETS_TICK"); ok {
+		config.TickDuration, err = time.ParseDuration(tickDuration)
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	if signingKey, ok := os.LookupEnv("SECRETS_JWT_SIGNING_KEY"); ok {
+		config.SigningKey = []byte(signingKey)
+	}
+
+	server := grpc.NewServer()
+	service := secrets.NewService(secrets.NewSecretStore(), config)
+
+	infrapb.RegisterSecretsServer(server, service)
 
 	log.Fatalln(server.Serve(listener))
 }
